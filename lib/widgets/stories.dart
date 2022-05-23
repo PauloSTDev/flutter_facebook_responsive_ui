9f8c19533b3b3f9b9a5038727692d71cc09f221c
paulo.santosDell <afonso.mea@gmail.com>
2022-05-23 15:56:23 -0300

    Adiciona Botão para adicionar Story

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
 
 class Stories extends StatelessWidget {
   final User currentUser;
@@ -19,17 +22,81 @@ class Stories extends StatelessWidget {
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
           padding: EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 8.0,
+            vertical: 10.0,
+            horizontal: 8.0,
           ),
           itemCount: 1 + stories.length,
           itemBuilder: (BuildContext context, int index) {
-            if (index == 0){
-              return Padding(padding: EdgeInsets.symmetric(horizontal: 4.0),
-
+            if (index == 0) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                child: _StoryCard(
+                  isAddStory: true,
+                  currentUser: currentUser,
+                ),
               );
             }
+            final Story story = stories[index - 1];
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 4.0),
+              child: _StoryCard(
+                story: story,
+              ),
+            );
           }),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  final bool isAddStory;
+  final User currentUser;
+  final Story story;
+
+  const _StoryCard(
+      {Key key, this.isAddStory = false, this.currentUser, this.story})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: CachedNetworkImage(
+            imageUrl: isAddStory ? currentUser.imageUrl : story.imageUrl,
+            height: double.infinity,
+            width: 110.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          height: double.infinity,
+          width: 110.0,
+          decoration: BoxDecoration(
+              gradient: Palette.storyGradient,
+              borderRadius: BorderRadius.circular(12.0)),
+        ),
+        Positioned(
+          top: 8.0,
+          left: 8.0,
+          child: isAddStory
+              ? Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: Icon(Icons.add),
+                    iconSize: 30.0,
+                    color: Palette.facebookBlue,
+                    onPressed: () => print("Add to Story"),
+                  ),
+                )
+              : ProfileAvatar(imageUrl: story.imageUrl),
+        ),
+      ],
     );
   }
 }
