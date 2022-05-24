37ba407b0c12931b1ebdeb4a5cd7509834f6921b
paulo.santosDell <afonso.mea@gmail.com>
2022-05-24 14:52:13 -0300

    Cria post_container.dart e apresenta em home_page.dart

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/data/data.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/circle_button.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/create_post_container.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/post_container.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/rooms.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/stories.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -53,6 +55,15 @@ class HomePage extends StatelessWidget {
             padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
             sliver: SliverToBoxAdapter(
               child: Stories(currentUser: currentUser, stories: stories),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              final Post post = posts[index];
+              return PostContainer(post: post);
+
+            },
+            childCount: posts.length,
             ),
           ),
         ],
