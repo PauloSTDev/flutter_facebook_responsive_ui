33b4471f2a34fa187ec2bd0e3843f4b032ef0c73
paulo.santosDell <afonso.mea@gmail.com>
2022-05-30 11:08:11 -0300

    Resolve bug do ShaderMask

--- lib/widgets/rooms.dart
+++ lib/widgets/rooms.dart
@@ -54,14 +54,10 @@ class _CreateRoomButton extends StatelessWidget {
       onPressed: () => print("Create Room"),
       child: Row(
         children: [
-          ShaderMask(
-            shaderCallback: (rect) =>
-                Palette.createRoomGradient.createShader(rect),
-            child: Icon(
-              Icons.video_call,
-              size: 35.0,
-              color: Colors.white,
-            ),
+          Icon(
+            Icons.video_call,
+            size: 35.0,
+            color: Colors.purple,
           ),
           SizedBox(
             width: 4.0,
